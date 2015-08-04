d828c7657d700a3e94742462a077539e4835809a
Ana Baraldi <avbaraldi@gmail.com>
2015-08-04 15:03:51 -0300

    finisehed class

--- classes.js
+++ classes.js
@@ -1,3 +1,22 @@
+for(var i = 0; i<9; i ++) { console.log(i); }
+
+// transform < > in harmless strings
+var html = '<script src="http://hackyourwebsite.com/eviljavascript.js"></script>';
+
+var charEscape = function(_html) {
+    var newHTML = _html;
+    // How will you make sure that newHTML doesn't contain any < or > ?
+    // Your code goes here!
+    newHTML = newHTML.replace(/</g, "");
+    newHTML = newHTML.replace(/>/g, "");
+
+    // Don't delete this line!
+    return newHTML;
+};
+
+// Did your code work? The line below will tell you!
+charEscape(html);
+
 // String Manipulation #1
 
 var s = "audacity";
@@ -37,3 +56,59 @@ function nameChanger(oldName) {
 
 // Did your code work? The line below will tell you!
 nameChanger(name);
+
+// Object Notation
+// Literal notation
+
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contactInfo" : contactInfo,
+	"welcomeMessage" : welcomeMessage,
+	"pictureURL" : pictureURL,
+	"skills" : skills
+};
+
+// dot notation
+var work = {};
+work.position = "Economist";
+work.employer = "Vérios Investimentos";
+work.years = "1,5 years";
+work.city = "São Paulo";
+
+// brackets notation
+var education = {};
+
+education["name"] = "Unicamp";
+education["years"] = "5 years";
+education["city"] = "Campinas";
+
+// json notation
+
+var education = {
+	"schools" : [
+	{
+		"name" : "Unicamp",
+		"city" : "Campinas"
+	},
+	{
+		"name" : "John Hopkins",
+		"city" : "Coursera - Internet"
+	}
+	]
+};
+
+// example of while loop, it will only stops when cameron is no long a course dev or we have a max
+var cameron = {};
+cameron.job = "course dev";
+
+var courses = 0;
+while(cameron.job === "course dev") {
+	makeCourse();
+	courses = courses + 1;
+	if(courses === 10) {
+		cameron.job === "learning specialist";
+	}
+}
+
+
